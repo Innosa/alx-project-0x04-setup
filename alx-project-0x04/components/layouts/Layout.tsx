9d1f88918151fef32e716c0f51229ed7f3b19f5e
Innosa <innocentokafo@gmail.com>
2025-11-29 12:47:26 +0100

    Final submission: React Context API project with working counter

--- alx-project-0x04/components/layouts/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { LayoutProps } from "@/interfaces";
-
-export default function Layout({ children }: LayoutProps) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      {children}
-    </div>
-  );
-}
